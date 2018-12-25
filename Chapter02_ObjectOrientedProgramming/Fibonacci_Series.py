569fde26ac5b0df77f353cf61e7300da267f327e
absognety <cvikas10@ms.ds.uhc.com>
2018-12-25 14:45:41 +0530

    Fibonacci Series class

---
+++ Chapter02_ObjectOrientedProgramming/Fibonacci_Series.py
@@ -0,0 +1,29 @@
+from Progression_Example import Progression
+
+class Fibonacci_Series(Progression):
+    """
+    Iterator producing a generalized Fibonacci progression.
+    """
+    
+    def __init__(self, first=0, second=1):
+        """
+        Create a new fibonacci progression.
+        first the first term of the progression (default 0)
+        second the second term of the progression (default 1)
+        """
+        super().__init__(first) # start progression at first
+        self._prev = second-first # fictitious value preceding the first
+        self._series = [self._current,self._prev]
+        
+    def advance(self):
+            """
+            Update current value by taking sum of previous two.
+            """
+            self._prev, self._current = self._current, self._prev + self._current
+            self._series.append(self._current)
+                    
+    def __getitem__(self):
+            return (self._series)
+        
+        
+#Code Fragment 2.11: A class that produces a Fibonacci progression.
