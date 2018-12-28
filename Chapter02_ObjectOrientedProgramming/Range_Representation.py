7cde066c4c92eae3fb697b1e280e840055dc582e
absognety <cvikas10@ms.ds.uhc.com>
2018-12-28 23:02:20 +0530

    indent fix (__init__ and __len__ methods)

--- Chapter02_ObjectOrientedProgramming/Range_Representation.py
+++ Chapter02_ObjectOrientedProgramming/Range_Representation.py
@@ -5,18 +5,19 @@ class Range():
     
     """
     def __init__(self,start=0,stop=None,step=1):
-       
-       if (step==0):
+        
+        
+        if (step==0):
            raise ValueError('Step cannot be Zero')
            
-       if stop is None:
-           start,stop=0,start
+        if stop is None:
+            start,stop=0,start
            
-       self._length = max(0,(stop-start+step-1)//step)
+        self._length = max(0,(stop-start+step-1)//step)
        
-       # need knowledge of start and step (but not stop) to support getitem
-       self._start=start
-       self._step=step
+        # need knowledge of start and step (but not stop) to support getitem
+        self._start=start
+        self._step=step
     
     def __len__(self):
         return (self._length)
