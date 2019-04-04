c82d079967955a47ba08f6b7dc6b02f779bdf7ce
absognety <vikasch.1994@gmail.com>
2019-04-04 16:08:27 +0530

    chapter 4 Recursion - code fragments

---
+++ Chapter04_Recursion/factorialFunction.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return (n * factorial(n-1))
+    
+    
+#Code Fragment 4.1: A recursive implementation of the factorial 
+#function
